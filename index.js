3c0fd3e021a1107cfa5f98dbe83b24516c8a427c
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-10-19 20:21:33 -0300

    feat: calc

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require(`express`);
+const app = express();
+
+app.use(express.json());
+
+app.post(`/sum`, (req, res) => {
+  const {a,b} = req.body;
+  const result = Number(a) + Number(b);
+  res.json(result);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`server is up and running at port ${port}`);
+});
